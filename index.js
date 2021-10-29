6a10b4985bb27af7a101a0b993fe460db691321b
armanh20439 <armanh20439@gmail.com>
2021-10-29 12:15:28 +0600

    mongodb-dataload

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 7000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j0bnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function  run(){
+
+    try{
+        await client.connect()
+        console.log('hitting the database')
+        const database = client.db("tourism");
+    const serviceCollection = database.collection("services");
+    // post api
+
+    app.post('/addUser', async (req, res) => {
+        const service = req.body;
+        console.log('hit the post api', service);
+
+        const result = await serviceCollection.insertOne(service);
+        console.log(result);
+        res.json(result)
+    });
+
+    }
+finally{
+
+}
+
+}
+
+run().catch(console.dir)
+
+app.get('/',(req,res)=>{
+res.send('welcom to tourism dabase')
+})
+
+app.listen(port, () => {
+    console.log('Running Genius Server on port', port);
+})
